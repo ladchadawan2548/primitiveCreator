ae9bf08e20d02008f243c1534bd63e7bfb927e73
Your Name <you@example.com>
2025-09-27 13:22:17 +0700

    comment

--- primitiveCreatorUI.py
+++ primitiveCreatorUI.py
@@ -1,21 +1,65 @@
 try:
+    from PySide6 import QtCore, QtGui, QtWidgets
+    from shiboken6 import wrapInstance
+except:
     from PySide2 import QtCore, QtGui, QtWidgets
     from shiboken2 import wrapInstance
 
-except :
-    from PySide import QtCore, QtGui, QtWidgets
-    from shiboken import wrapInstance
-
 import maya.OpenMayaUI as omui
+import os
+
+ICON_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'icons'))
 
 class PrimitiveCreatorDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-
         
         self.resize(380, 300)
-        self.setWindowTitle("Primitive Creator")
- 
+        self.setWindowTitle('Primitive Creator 💚')
+
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+
+        # ✅ สร้าง list widget แค่ครั้งเดียว
+        self.primitive_listWidget = QtWidgets.QListWidget()
+        self.primitive_listWidget.setIconSize(QtCore.QSize(50,50))
+        self.primitive_listWidget.setSpacing(8)
+        self.primitive_listWidget.setViewMode(QtWidgets.QListView.IconMode)
+        self.primitive_listWidget.setMovement(QtWidgets.QListView.Static)
+        self.primitive_listWidget.setResizeMode(QtWidgets.QListView.Adjust)
+        self.main_layout.addWidget(self.primitive_listWidget)
+
+        # Name layout
+        self.name_layout = QtWidgets.QHBoxLayout()
+        self.main_layout.addLayout(self.name_layout)
+
+        self.name_label = QtWidgets.QLabel('Name:')
+        self.name_lineEdit = QtWidgets.QLineEdit()
+        self.name_layout.addWidget(self.name_label)
+        self.name_layout.addWidget(self.name_lineEdit)
+
+        # Buttons
+        self.button_layout = QtWidgets.QHBoxLayout()
+        self.main_layout.addLayout(self.button_layout)
+
+        self.create_button = QtWidgets.QPushButton('🪼🐠Create')
+        self.cancel_button = QtWidgets.QPushButton('❌👻Cancel')
+        self.cancel_button.clicked.connect(self.close)
+
+        self.button_layout.addStretch()
+        self.button_layout.addWidget(self.create_button)
+        self.button_layout.addWidget(self.cancel_button)
+
+        # Load icons
+        self.initIconWidgets()
+
+    def initIconWidgets(self):
+        prims = ['cone', 'cube', 'sphere', 'torus']
+        for prim in prims:
+            item = QtWidgets.QListWidgetItem(prim)
+            item.setIcon(QtGui.QIcon(os.path.join(ICON_PATH, f'{prim}.png')))
+            self.primitive_listWidget.addItem(item)
+
+
 
 def run():
     global ui
